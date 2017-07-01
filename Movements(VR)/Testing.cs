cb145e601481a1c3d7132bbdf04f0906760c59c1
Nick0108 <zidonglin@outlook.com>
2017-07-01 22:56:21 +0800

    Feat: the frequently used movements in VR

---
+++ Movements(VR)/Testing.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testing : MonoBehaviour
+{
+
+    void Start()
+    {
+        Debug.Log("start1");
+        StartCoroutine(Test());
+        Debug.Log("start2");
+    }
+
+    IEnumerator Test()
+    {
+        Debug.Log("test1");
+        yield return StartCoroutine(DoSomething());
+        Debug.Log("test2");
+    }
+
+    IEnumerator DoSomething()
+    {
+        Debug.Log("load 1");
+        yield return StartCoroutine(ForMoreThing());
+        Debug.Log("load 2");
+    }
+
+    IEnumerator ForMoreThing()
+    {
+        Debug.Log("More 1");
+        yield return StartCoroutine(ForlastThing());
+        Debug.Log("More 2");
+    }
+
+    IEnumerator ForlastThing()
+    {
+        Debug.Log("Last 1");
+        yield return null;
+        Debug.Log("Last 2");
+    }
+}
